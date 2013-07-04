41da47b7e85ebecb1c8541e27d9455eb9a37d169
Jakub Lekstan <kuebzky@gmail.com>
2013-07-04 11:06:42 +0200

    Uncompressing

---
+++ buffer.js
@@ -0,0 +1,51 @@
+var compress_buffer = require("compress-buffer-crc32");
+var crc_utils = require("crc-utils2");
+
+var a = compress_buffer.compress(new Buffer("a"), -1);
+var b = compress_buffer.compress(new Buffer("b"), -1);
+var ab = compress_buffer.compress(new Buffer("ab"), -1);
+
+var crc = crc_utils.crc32_combine_multi([a,b]);
+
+console.log("BUFOR A", a);
+console.log("BUFOR B", b);
+console.log("BUFOR AB", ab);
+console.log("CRC", crc);
+
+var a_body = a.slice(10, a.length - 10);
+var b_body = b.slice(10, b.length - 10);
+var ab_body = ab.slice(10, ab.length - 10);
+
+var gzipHeader = new Buffer([0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03]);
+var gzipEnding = new Buffer([0x03, 0x00]);
+
+console.log("BODY A", a_body);
+console.log("BODY B", b_body);
+console.log("BODY AB", ab_body);
+
+var length = gzipHeader.length + a_body.length + b_body.length + gzipEnding.length + crc.combinedCrc32.length + crc.bufferLength.length;
+var end = 0;
+var d = new Buffer(length);
+
+gzipHeader.copy(d, end);
+end += gzipHeader.length;
+
+a_body.copy(d, end);
+end += a_body.length;
+
+b_body.copy(d, end);
+end += b_body.length;
+
+gzipEnding.copy(d, end);
+end += gzipEnding.length;
+
+crc.combinedCrc32.copy(d, end);
+end += crc.combinedCrc32.length;
+
+crc.bufferLength.copy(d, end);
+end += crc.bufferLength.length;
+
+console.log("DLUGOSC", length, end);
+console.log(d);
+
+console.log("ODKOMPRES", compress_buffer.uncompress(d));
--- src/compress-buffer.cc
+++ src/compress-buffer.cc
@@ -93,8 +93,9 @@ Handle<Value> compress(const Arguments& args) {
 	return scope.Close(BufferOut->handle_);
 }
 	
-
 Handle<Value> uncompress(const Arguments &args) {
+	HandleScope scope;
+
 	if (args.Length() < 1) {
 		return Undefined();
 	}
@@ -128,15 +129,18 @@ Handle<Value> uncompress(const Arguments &args) {
 	uint32_t malloc_size=0;
 	uint32_t currentPosition=0;
 	
-	int ret; 
-	
-	do {
+	int ret;
+	int pos;
+	int last = 0;
+
+	for (;;) {
 		Bytef *tmp = (Bytef*)malloc(CHUNK);
 		strmUncompress.avail_out = CHUNK;
 		strmUncompress.next_out = tmp;
 
-		ret = inflate(&strmUncompress, Z_NO_FLUSH);
+		ret = inflate(&strmUncompress, Z_BLOCK);
 		assert(ret != Z_STREAM_ERROR); 
+
 		switch (ret) {
 			case Z_NEED_DICT:
 				ret = Z_DATA_ERROR;     /* and fall through */
@@ -151,31 +155,123 @@ Handle<Value> uncompress(const Arguments &args) {
 				}
 				return Undefined();
 		}
-		
+
+		if (strmUncompress.data_type & 128) {
+			if (last) {
+				break;
+			}
+
+			pos = strmUncompress.data_type & 7;
+			if (pos != 0) {
+				last = strmUncompress.next_in[-1] & (0x100 >> pos);
+			} else {
+				last = strmUncompress.next_in[0] & 1;
+			}
+		}
+
 		uint32_t have = CHUNK - strmUncompress.avail_out;
 		if (have>0) {
 			bufferOut = (Bytef *) realloc(bufferOut, malloc_size+have);
 			malloc_size=malloc_size+have;
-		}
 		
-		memcpy(bufferOut+currentPosition, tmp, have);
-		currentPosition+=have;
+			memcpy(bufferOut+currentPosition, tmp, have);
+			currentPosition+=have;
+		}
+
 		free(tmp);
-	} while (strmUncompress.avail_out == 0 && ret != Z_STREAM_END);
+	}
 	
 	inflateEnd(&strmUncompress);
-
-	if (ret != Z_STREAM_END) { 
-		if (bufferOut!=NULL) { 
-			free(bufferOut);
-		} 
-		return Undefined();
-	}
 
 	Buffer *BufferOut=Buffer::New((char *)bufferOut, malloc_size);
 	free(bufferOut);
 
+	return scope.Close(BufferOut->handle_);
+}
+
+Handle<Value> uncompress2 (const Arguments &args) {
 	HandleScope scope;
+
+	if (args.Length() < 1) {
+		return Undefined();
+	}
+
+	if (!Buffer::HasInstance(args[0])) {
+		ThrowNodeError("First argument must be a Buffer");
+		return Undefined();
+	}
+
+	z_stream strmUncompress;
+
+	strmUncompress.zalloc=Z_NULL;
+	strmUncompress.zfree=Z_NULL;
+	strmUncompress.opaque=Z_NULL;
+	strmUncompress.avail_in = 0;
+	strmUncompress.next_in = Z_NULL;
+
+	int rci = inflateInit2(&strmUncompress, WBITS);
+
+	if (rci != Z_OK) {
+		ThrowNodeError("zlib initialization error.");
+		return Undefined();
+	}
+
+	Local<Object> bufferIn=args[0]->ToObject();
+
+	strmUncompress.next_in = (Bytef*) Buffer::Data(bufferIn);
+	strmUncompress.avail_in = Buffer::Length(bufferIn);
+
+	Bytef  *bufferOut = NULL;
+	uint32_t malloc_size=0;
+	uint32_t currentPosition=0;
+
+	int ret;
+
+	do {
+		Bytef *tmp = (Bytef*)malloc(CHUNK);
+		strmUncompress.avail_out = CHUNK;
+		strmUncompress.next_out = tmp;
+
+		ret = inflate(&strmUncompress, Z_BLOCK);
+		assert(ret != Z_STREAM_ERROR);
+
+		switch (ret) {
+			case Z_NEED_DICT:
+				ret = Z_DATA_ERROR;     /* and fall through */
+			case Z_DATA_ERROR:
+			case Z_MEM_ERROR:
+				(void)inflateEnd(&strmUncompress);
+				if (bufferOut!=NULL) {
+					free(bufferOut);
+				}
+				if (tmp!=NULL) {
+					free(tmp);
+				}
+				return Undefined();
+		}
+
+
+		if (strmUncompress.data_type & 64) {
+			break;
+		}
+
+		uint32_t have = CHUNK - strmUncompress.avail_out;
+		if (have>0) {
+			bufferOut = (Bytef *) realloc(bufferOut, malloc_size+have);
+			malloc_size=malloc_size+have;
+
+			memcpy(bufferOut+currentPosition, tmp, have);
+			currentPosition+=have;
+		}
+
+		free(tmp);
+	} while (ret == Z_OK);
+
+	inflateEnd(&strmUncompress);
+
+	Buffer *BufferOut=Buffer::New((char *)bufferOut, malloc_size);
+	free(bufferOut);
+
 	return scope.Close(BufferOut->handle_);
 }
 
@@ -186,7 +282,7 @@ init (Handle<Object> target) {
 	strmCompress.opaque=Z_NULL;
 
 	int rcd = deflateInit2(&strmCompress, Z_DEFAULT_COMPRESSION, Z_DEFLATED,  
-		WBITS, 8L,  Z_DEFAULT_STRATEGY);
+		WBITS, 8L, Z_DEFAULT_STRATEGY);
 
 	if (rcd != Z_OK) {
 		ThrowNodeError("zlib initialization error.");
@@ -195,6 +291,7 @@ init (Handle<Object> target) {
 
 	NODE_SET_METHOD(target, "compress", compress);
 	NODE_SET_METHOD(target, "uncompress", uncompress);
+	NODE_SET_METHOD(target, "uncompress2", uncompress2);
 }
 
 NODE_MODULE(compress_buffer_bindings, init);
